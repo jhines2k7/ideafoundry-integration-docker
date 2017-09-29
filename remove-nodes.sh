8675175ba6744e459b18d5821ac49867a3566f77
James Hines <jameshines10@gmail.com>
2017-09-29 09:25:42 -0400

    Fixes service names

--- remove-nodes.sh
+++ remove-nodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in legacydataexport processorderemail createperson createorder createquestion mailsource kafka mysql; 
+for i in legacydataexport processorderemail createperson createorder createquestion mailsource if-kafka if-mysql; 
     do docker-machine rm -f $i; 
 done
