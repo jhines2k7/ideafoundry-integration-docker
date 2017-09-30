aa2efc10dcefaaf0d9f44f20e302bcc75681b049
James Hines <jameshines10@gmail.com>
2017-09-30 11:25:47 -0400

    Adds a compose file for starting a mysql service

--- stop-nodes.sh
+++ stop-nodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in legacydataexport processorderemail createperson createorder createquestion mailsource if-kafka if-mysql; 
+for i in legacydataexport processorderemail createperson createorder createquestion mailsource if-kafka if-mysql swarm-manager; 
     do docker-machine stop $i; 
 done
