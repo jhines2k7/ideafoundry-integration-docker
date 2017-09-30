5bc2ccf47e2c71ade77b903e3b6adee967b304a8
James Hines <jameshines10@gmail.com>
2017-09-30 14:52:03 -0400

    Adds a script for starting nodes

--- start-nodes.sh
+++ start-nodes.sh
@@ -2,5 +2,5 @@
 
 #for i in legacydataexport processorderemail createperson createorder createquestion mailsource if-kafka if-mysql if-swarm-manager; 
 for i in mailsource if-kafka if-mysql if-swarm-manager; 
-    do docker-machine stop $i; 
+    do docker-machine start $i; 
 done
