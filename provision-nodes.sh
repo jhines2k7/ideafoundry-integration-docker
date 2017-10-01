1586fa346aef474a160f9ade0762e7a48a7cf4f9
James Hines <jameshines10@gmail.com>
2017-10-01 11:15:31 -0400

    Changes the size of the service worker and swarm nodes to 1gb

--- provision-nodes.sh
+++ provision-nodes.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN if-swarm-manager;
-
-for i in legacydataexport processorderemail createperson createorder createquestion mailsource;
-    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN $i; 
+for i in if-swarm-manager legacydataexport processorderemail createperson createorder createquestion mailsource;
+    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN $i; 
 done
 
 for i in if-kafka if-mysql;
