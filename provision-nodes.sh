036d7804fdf2e01d8de230771357d9372f37541e
James Hines <jameshines10@gmail.com>
2017-10-01 11:35:03 -0400

    Created a file called create-nodes.sh for creating nodes. Will use provision-nodes.sh for... provisioning nodes!

--- provision-nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in if-swarm-manager legacydataexport processorderemail createperson createorder createquestion mailsource;
-    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN $i; 
-done
-
-for i in if-kafka if-mysql;
-    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN $i;
-done
