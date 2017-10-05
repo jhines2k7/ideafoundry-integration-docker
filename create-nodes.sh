ea6610790f7074ab9e400ff7e4f7766e75c3799f
James Hines <jameshines10@gmail.com>
2017-10-05 13:02:50 -0400

    Removes unused compose files. Updates create nodes script

--- create-nodes.sh
+++ create-nodes.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #create 1gb worker nodes
 for i in {1..20};
-    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN 1gbworker$i; 
+    do docker-machine create --engine-label node.type=1gbworker --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN 1gbworker$i; 
 done
 
-#create 2gb worker nodes
-docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN 2gbworker; 
+#create 2gb mysql nodes
+docker-machine create --engine-label node.type=2gbworker --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN mysql; 
+
+#create 2gb kafka node
+docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN kafka;
 
 #create manager node
-docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN manager;
+docker-machine create --swarm-master --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN manager;
