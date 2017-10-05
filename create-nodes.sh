3de476a0d5b9aa92a4a8951b22ab3d78b0b4c685
James Hines <jameshines10@gmail.com>
2017-10-05 13:19:28 -0400

    Updates the script for creating worker nodes

--- create-nodes.sh
+++ create-nodes.sh
@@ -4,11 +4,10 @@ for i in {1..20};
     do docker-machine create --engine-label node.type=1gbworker --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN 1gbworker$i; 
 done
 
-#create 2gb mysql nodes
-docker-machine create --engine-label node.type=2gbworker --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN mysql; 
-
-#create 2gb kafka node
-docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN kafka;
+#create kafka and mysql nodes
+for i in mysql kafka;
+    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN $i;
+done
 
 #create manager node
-docker-machine create --swarm-master --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN manager;
+docker-machine create --swarm-master --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN manager;
--- remove-nodes.sh
+++ remove-nodes.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for i in legacydataexport processorderemail createperson createorder createquestion mailsource if-kafka if-mysql if-swarm-manager; 
-    do docker-machine rm -f $i; 
+#remove 1gb worker nodes
+for i in {1..20}; 
+    do docker-machine rm -f 1gbworker$i; 
+done
+
+#remove 2gb worker node
+
+#remove mysql and kafka nodes
+for i in mysql kafka; 
+    do docker-machine rm -f $i;
 done
