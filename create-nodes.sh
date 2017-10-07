6dafe2e059486885005dc450aa4762120583ecd0
James Hines <jameshines10@gmail.com>
2017-10-07 10:19:52 -0400

    Updates the compose file to work with the different types of worker nodes

--- create-nodes.sh
+++ create-nodes.sh
@@ -1,13 +1,62 @@
 #!/bin/bash
-#create 1gb worker nodes
-for i in {1..20};
-    do docker-machine create --engine-label node.type=1gbworker --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN 1gbworker$i; 
+#create createperson worker nodes
+for i in {1..4};
+    do docker-machine create \
+    --engine-label node.type=createperson \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size 1gb \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    createperson-worker-$i; 
+done
+
+#create createorder worker nodes
+for i in {1..4};
+    do docker-machine create \
+    --engine-label node.type=createorder \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size 1gb \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    createorder-worker-$i; 
+done
+
+#create createquestion worker nodes
+for i in {1..12};
+    do docker-machine create \
+    --engine-label node.type=createquestion \
+    --driver digitalocean --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size 1gb \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    createoquestion-worker-$i; 
 done
 
 #create kafka and mysql nodes
 for i in mysql kafka;
-    do docker-machine create --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 2gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN $i;
+    do docker-machine create \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size 2gb \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    $i;
+done
+
+#create 1gb worker nodes
+for i in {1..3};
+    do docker-machine create \
+    --engine-label node.type=1gb-worker \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size 1gb \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    1gb-worker-$i; 
 done
 
 #create manager node
-docker-machine create --swarm-master --driver digitalocean --digitalocean-image ubuntu-17-04-x64 --digitalocean-size 1gb --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN manager;
+docker-machine create \
+--swarm-master \
+--driver digitalocean \
+--digitalocean-image ubuntu-17-04-x64 \
+--digitalocean-size 1gb \
+--digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+manager;
--- remove-nodes.sh
+++ remove-nodes.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-#remove 1gb worker nodes
-for i in {1..20}; 
-    do docker-machine rm -f 1gbworker$i; 
+#remove createperson worker nodes
+for i in {1..4};
+    do docker-machine rm -f createperson-worker-$i; 
 done
 
-#remove 2gb worker node
+#remove createorder worker nodes
+for i in {1..4};
+    do docker-machine rm -f createorder-worker-$i; 
+done
 
-#remove mysql and kafka nodes
-for i in mysql kafka; 
+#remove createquestion worker nodes
+for i in {1..12};
+    do docker-machine rm -f createoquestion-worker-$i; 
+done
+
+#remove kafka and mysql nodes
+for i in mysql kafka;
     do docker-machine rm -f $i;
 done
+
+#remove manager node
+docker-machine rm -f manager;
