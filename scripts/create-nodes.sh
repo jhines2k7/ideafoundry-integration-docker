fb634c72c048a0a78a1d4be9aae48ef6f6a485f5
James Hines <jameshines10@gmail.com>
2017-10-08 03:55:36 -0400

    Refactors the node creation script

--- scripts/create-nodes.sh
+++ scripts/create-nodes.sh
@@ -3,13 +3,14 @@
 function create_worker_node {
     node=$1
     label=$2
+    size=$3
 
     echo " ======> creating $node worker node"
     docker-machine create \
         --engine-label $label \
         --driver digitalocean \
         --digitalocean-image ubuntu-17-04-x64 \
-        --digitalocean-size 1gb \
+        --digitalocean-size $size \
         --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
         $node
 }
@@ -17,32 +18,21 @@ function create_worker_node {
 #create createperson worker nodes
 for i in {0..3};
     do 
-         create_worker_node createperson-worker-$i "node.type=createperson"
+         create_worker_node createperson-worker-$i "node.type=createperson" 1gb
 done
 
 #create createorder worker nodes
 # echo " ======> creating createorder worker nodes"
 # for i in {0..3};
 #     do 
-#         docker-machine create \
-#         --engine-label node.type=createorder \
-#         --driver digitalocean \
-#         --digitalocean-image ubuntu-17-04-x64 \
-#         --digitalocean-size 1gb \
-#         --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
-#         createorder-worker-$i; 
+#         create_worker_node createorder-worker-$i "node.type=createorder" 1gb
 # done
 
 #create createquestion worker nodes
 #echo " ======> creating createquestion worker nodes"
 # for i in {0..11};
 #     do 
-#         docker-machine create \
-#         --engine-label node.type=createquestion \
-#         --driver digitalocean --digitalocean-image ubuntu-17-04-x64 \
-#         --digitalocean-size 1gb \
-#         --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
-#         createoquestion-worker-$i; 
+#         create_worker_node createquestion-worker-$i "node.type=createquestion" 1gb
 # done
 
 #create kafka and mysql nodes
@@ -61,13 +51,7 @@ done
 echo " ======> creating 1gb worker nodes"
 for i in {0..2};
     do 
-        docker-machine create \
-        --engine-label node.type=1gb-worker \
-        --driver digitalocean \
-        --digitalocean-image ubuntu-17-04-x64 \
-        --digitalocean-size 1gb \
-        --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
-        1gb-worker-$i; 
+         create_worker_node 1gb-worker-$i "node.type=1gb-worker" 1gb
 done
 
 #create manager node
