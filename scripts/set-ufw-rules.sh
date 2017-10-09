0f9944359e3d65a22dcc14edec1589cfde705b33
James Hines <jhines2k7@yahoo.com>
2017-10-09 12:58:38 -0400

    Update set-ufw-rules.sh
    Sudo

--- scripts/set-ufw-rules.sh
+++ scripts/set-ufw-rules.sh
@@ -4,10 +4,10 @@
 
 echo "======> setting up firewall rules for $1 ..."
 docker-machine ssh $1 \
-ufw allow 22/tcp \
-&& ufw allow 2376/tcp \
-&& ufw allow 2377/tcp \
-&& ufw allow 7946/tcp \
-&& ufw allow 7946/udp \
-&& ufw allow 4789/udp \
-&& echo "y" | ufw enable
+sudo ufw allow 22/tcp \
+&& sudo ufw allow 2376/tcp \
+&& sudo ufw allow 2377/tcp \
+&& sudo ufw allow 7946/tcp \
+&& sudo ufw allow 7946/udp \
+&& sudo ufw allow 4789/udp \
+&& echo "y" | sudo ufw enable
