3be9aedbc0be5bad430bc78077c891d8ff4d5404
James Hines <jhines2k7@yahoo.com>
2017-10-09 12:38:35 -0400

    Update set-ufw-rules.sh
    Calls docker-machine ssh with the correct value

--- scripts/set-ufw-rules.sh
+++ scripts/set-ufw-rules.sh
@@ -3,7 +3,7 @@
 # set ufw rules for node
 
 echo "======> setting up firewall rules for $1 ..."
-docker-machine ssh $machine \
+docker-machine ssh $1 \
 ufw allow 22/tcp \
 && ufw allow 2376/tcp \
 && ufw allow 2377/tcp \
