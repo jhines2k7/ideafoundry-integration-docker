f4022f9d8c6cbab9938c430666d47e4ad0045df5
James Hines <jhines2k7@yahoo.com>
2017-10-09 12:16:02 -0400

    Update set-ufw-rules.sh
    Not necessary to call a function here

--- scripts/set-ufw-rules.sh
+++ scripts/set-ufw-rules.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 # set ufw rules for node
-function set_ufw_rules {
-    local machine=$1
 
-    echo "======> setting up firewall rules for $machine ..."
-    docker-machine ssh $machine \
-    ufw allow 22/tcp \
-    && ufw allow 2376/tcp \
-    && ufw allow 2377/tcp \
-    && ufw allow 7946/tcp \
-    && ufw allow 7946/udp \
-    && ufw allow 4789/udp \
-    && echo "y" | ufw enable
-}
-
-set_ufw_rules $1
+echo "======> setting up firewall rules for $1 ..."
+docker-machine ssh $machine \
+ufw allow 22/tcp \
+&& ufw allow 2376/tcp \
+&& ufw allow 2377/tcp \
+&& ufw allow 7946/tcp \
+&& ufw allow 7946/udp \
+&& ufw allow 4789/udp \
+&& echo "y" | ufw enable
