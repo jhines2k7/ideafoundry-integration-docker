cba0c07ac6df6d684a721f3a9eaeeae2e4a5dc4c
James Hines <jameshines10@gmail.com>
2017-10-09 08:40:54 -0400

    Adds create_worker_node utility function to the bash scripts

---
+++ scripts/create-worker-node.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+function create_node {
+    local machine=$1
+    local label=$2
+    local size=$3
+    local ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+
+    echo " ======> creating $machine-$ID node"
+    docker-machine create \
+    --engine-label $label \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size $size \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    $machine-$ID
+}
+
+function create_worker_node {
+    local machine=$1
+    local label=$2
+    local size=$3
+    local num_workers=$4
+    local ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+
+
+    if [ $num_workers -gt 1 ]
+    then
+        echo "Creating $num_workers nodes"
+
+        for i in $(eval echo "{1..$num_workers}")
+            do
+                create_node $1 $2 $3                
+        done
+    else
+        echo "Creating $num_workers node"
+        create_node $1 $2 $3
+    fi
+}
+
+create_worker_node $1 $2 $3 $4
