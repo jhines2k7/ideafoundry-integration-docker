d5197fa758537980f8b6abfb4df7c87b29940398
James Hines <jhines2k7@yahoo.com>
2017-10-12 13:53:10 -0400

    Update set-manager-env-variables.sh

--- scripts/set-manager-env-variables.sh
+++ scripts/set-manager-env-variables.sh
@@ -2,24 +2,24 @@
 
 echo "=======> setting env variables for manager node"
         
-echo "export DB_HOST=$DB_HOST" >> /root/.profile
-echo "export KAFKA_HOST=$KAFKA_HOST" >> /root/.profile
-echo "export ZOOKEEPER_HOST=$ZOOKEEPER_HOST" >> /root/.profile
-echo "export OKHTTP_CLIENT_TIMEOUT_SECONDS=$OKHTTP_CLIENT_TIMEOUT_SECONDS" >> /root/.profile
-echo "export AIRTABLE_APP_ID=$AIRTABLE_APP_ID" >> /root/.profile
-echo "export OCCASION_EXPORT_STARTING_PAGE_NUM=$OCCASION_EXPORT_STARTING_PAGE_NUM" >> /root/.profile
-echo "export IF_OCCASION_EXPORT_URL=$IF_OCCASION_EXPORT_URL" >> /root/.profile
-echo "export IF_AIRTABLE_CREDS=$IF_AIRTABLE_CREDS" >> /root/.profile
-echo "export IF_DB_PASSWORD=$IF_DB_PASSWORD" >> /root/.profile
-echo "export IF_DB_PORT=$IF_DB_PORT" >> /root/.profile
-echo "export IF_DB_ROOT_PASS=$IF_DB_ROOT_PASS" >> /root/.profile
-echo "export IF_DB_USERNAME=$IF_DB_USERNAME" >> /root/.profile
-echo "export IF_EMAIL_CREDS=$IF_EMAIL_CREDS" >> /root/.profile
-echo "export IF_EMAIL_ID=$IF_EMAIL_ID" >> /root/.profile
-echo "export IF_OCCASION_CREDS=$IF_OCCASION_CREDS" >> /root/.profile
-echo "export GIT_USERNAME=$GIT_USERNAME" >> /root/.profile
-echo "export GIT_PASSWORD=$GIT_PASSWORD" >> /root/.profile
-echo "export DOCKER_USER=$DOCKED_USER" >> /root/.profile
-echo "export DOCKER_PASS=$DOCKER_PASS" >> /root/.profile
-echo "export DOCKER_HOST=$DOCKER_HOST" >> /root/.profile
-echo "export DIGITALOCEAN_ACCESS_TOKEN=$DIGITALOCEAN_ACCESS_TOKEN" >> /root/.profile
+echo "export DB_HOST=$1" >> /root/.profile
+echo "export KAFKA_HOST=$2" >> /root/.profile
+echo "export ZOOKEEPER_HOST=$3" >> /root/.profile
+echo "export OKHTTP_CLIENT_TIMEOUT_SECONDS=$4" >> /root/.profile
+echo "export AIRTABLE_APP_ID=$5" >> /root/.profile
+echo "export OCCASION_EXPORT_STARTING_PAGE_NUM=$6" >> /root/.profile
+echo "export IF_OCCASION_EXPORT_URL=$7" >> /root/.profile
+echo "export IF_AIRTABLE_CREDS=$8" >> /root/.profile
+echo "export IF_DB_PASSWORD=$9" >> /root/.profile
+echo "export IF_DB_PORT=$10" >> /root/.profile
+echo "export IF_DB_ROOT_PASS=$11" >> /root/.profile
+echo "export IF_DB_USERNAME=$12" >> /root/.profile
+echo "export IF_EMAIL_CREDS=$13" >> /root/.profile
+echo "export IF_EMAIL_ID=$14" >> /root/.profile
+echo "export IF_OCCASION_CREDS=$15" >> /root/.profile
+echo "export GIT_USERNAME=$16" >> /root/.profile
+echo "export GIT_PASSWORD=$17" >> /root/.profile
+echo "export DOCKER_USER=$18" >> /root/.profile
+echo "export DOCKER_PASS=$19" >> /root/.profile
+echo "export DOCKER_HOST=$20" >> /root/.profile
+echo "export DIGITALOCEAN_ACCESS_TOKEN=$21" >> /root/.profile
