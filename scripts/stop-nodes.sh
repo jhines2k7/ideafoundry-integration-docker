f88b5262d176fb2e73afcfd0bce04632c00e9c59
James Hines <jhines2k7@yahoo.com>
2017-10-12 17:37:28 -0400

    Delete stop-nodes.sh

--- scripts/stop-nodes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in legacydataexport processorderemail createperson createorder createquestion mailsource if-kafka if-mysql if-swarm-manager; 
-    do docker-machine stop $i; 
-done
