246f0cffc0d12aa16b23c16be005584e394037c3
James Hines <jhines2k7@yahoo.com>
2017-10-12 12:57:08 -0400

    Create deploy-stack.sh

---
+++ scripts/deploy-stack.sh
@@ -0,0 +1,7 @@
+git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com:jhines2k7/ideafoundry-integration-docker.git
+
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS \
+
+cd ideafoundry-integration-docker
+
+docker stack deploy --compose-file docker-compose.yml --with-registry-auth integration
