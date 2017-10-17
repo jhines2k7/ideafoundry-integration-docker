713de58fa188c7a318e4dfef7b79a8426493a8ca
James Hines <jhines2k7@yahoo.com>
2017-10-17 12:05:02 -0400

    Update set-ufw-rules.sh

--- scripts/set-ufw-rules.sh
+++ scripts/set-ufw-rules.sh
@@ -7,7 +7,7 @@ echo "======> setting up firewall rules for $1 ..."
 docker-machine ssh $1 \
 echo '"y" | ufw --force enable \
 && ufw default deny incoming \
-&& ufw allow 9418/tcp \
+&& ufw allow 443/tcp \
 && ufw allow 22/tcp \
 && ufw allow 2376/tcp \
 && ufw allow 2377/tcp \
