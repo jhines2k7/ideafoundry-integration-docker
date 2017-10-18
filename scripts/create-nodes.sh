97c7254d926b0f5542894069c398308d5cde389f
jhines2k7 <jameshines10@gmail.com>
2017-10-18 11:15:23 +0000

    Comments out service

--- scripts/create-nodes.sh
+++ scripts/create-nodes.sh
@@ -175,7 +175,7 @@ create_manager_node
 init_swarm_manager
 copy_compose_file
 #create_person_worker_nodes 4
-create_1gb_worker_nodes 1
+#create_1gb_worker_nodes 1
 create_mysql_and_kafka_nodes
 #copy_sql_schema
 deploy_stack
