cf235d497492661e4a5fcca40f0a132c3a7549a1
James Hines <jameshines10@gmail.com>
2017-10-18 07:16:21 -0400

    Removes call to change directory

--- scripts/deploy-stack.sh
+++ scripts/deploy-stack.sh
@@ -2,6 +2,4 @@
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 
-cd ideafoundry-integration-docker
-
 docker stack deploy --compose-file docker-compose.yml --with-registry-auth integration
