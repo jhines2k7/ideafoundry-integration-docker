c880e6bf1c8d06d97fe3faa95a57f6c42f517a64
James Hines <jhines2k7@yahoo.com>
2017-10-20 16:33:49 -0400

    Update create-node.sh

--- scripts/create-node.sh
+++ scripts/create-node.sh
@@ -74,10 +74,10 @@ create_node () {
         else
             echo "There was an error installing docker on $machine-$ID."
             
-            echo "$machine-$ID" >> $file
-            
-            continue 2
+            echo "$machine-$ID" >> $file            
         fi        
+        
+        continue
     fi
     
     if [ "$machine" = "mysql" ]
