38f9fda53a6e9a2757632780acda68f7145f7a43
James Hines <jhines2k7@yahoo.com>
2017-10-20 14:02:38 -0400

    Update create-nodes.sh

--- scripts/create-nodes.sh
+++ scripts/create-nodes.sh
@@ -80,8 +80,9 @@ function deploy_stack {
     docker-machine ssh $manager_machine docker login --username=$DOCKER_USER --password=$DOCKER_PASS
     
     docker-machine ssh $manager_machine docker stack deploy \
-            --compose-file ./docker-compose.yml \
-            --with-registry-auth integration
+            --compose-file docker-compose.yml \
+            --with-registry-auth \
+            integration
 }
 
 function set_scaling_env_variables {
