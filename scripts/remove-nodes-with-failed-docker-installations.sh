789e35f31d6b29132270eb7dd9cdbfe72e0bfd80
James Hines <jhines2k7@yahoo.com>
2017-10-20 16:12:24 -0400

    Create remove-nodes-with-failed-docker-installations.sh

---
+++ scripts/remove-nodes-with-failed-docker-installations.sh
@@ -0,0 +1,9 @@
+if [[ -s $file ]] ; then
+    echo "======> removing machines with failed docker installations ..."
+
+    while read machine || [[ -n $machine ]] ; do
+        docker-machine rm -f $machine
+    done < $file
+else
+    echo "======> there were no machines with failed docker installations ..."
+fi ;
