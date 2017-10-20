03ce591e06014d1ef50c1d5db77c58b962869fbd
James Hines <jhines2k7@yahoo.com>
2017-10-20 12:02:43 -0400

    Update and rename create-worker-node.sh to create-node.sh

---
+++ scripts/create-node.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+function get_ip {
+    echo $(docker-machine ip $1)
+}
+
+function get_manager_machine_name {
+    echo $(docker-machine ls --format "{{.Name}}" | grep 'manager')
+}
+
+function get_worker_token {
+    local manager_machine=$(get_manager_machine_name)
+    # gets swarm manager token for a worker node
+    echo $(docker-machine ssh $manager_machine docker swarm join-token worker -q)
+}
+
+function join_swarm {
+    local manager_machine=$(get_manager_machine_name)
+    
+    docker-machine ssh $1 \
+    docker swarm join \
+        --token $(get_worker_token) \
+        $(get_ip $manager_machine):2377
+}
+
+function create_node {
+    local machine=$1
+    local label=$2
+    local size=$3
+    local ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+    
+    echo "======> creating $machine-$ID node"
+    
+    docker-machine create \
+    --engine-label $label \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-04-x64 \
+    --digitalocean-size $size \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    $machine-$ID
+    
+    if [ ! -e "$file" ] ; then
+        touch "$file"
+    fi
+    
+    #check to make sure docker was properly installed on node
+    echo "======> making sure docker is installed on $machine-$ID"
+    docker-machine ssh $machine-$ID docker
+
+    if [ $? -ne 0 ]
+    then
+        if [ $machine == "manager" ]
+        then
+            docker-machine rm -f $machine-$ID
+            echo "There was an error installing docker on the manager node. The script will now exit."
+            set -e
+        else
+            echo "There was an error installing docker on $machine-$ID."
+            echo "$machine-$ID" >> $file
+        fi        
+    fi           
+ 
+    sh ./set-ufw-rules.sh $machine-$ID
+    
+    if [ $machine != "manager" ]
+    then
+        join_swarm $machine-$ID
+    fi
+}
+
+local machine=$1
+local label=$2
+local size=$3
+local num_workers=$4
+local ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+
+if [ $num_workers -gt 1 ]
+then
+    echo "======> Creating $num_workers nodes"
+
+    for i in $(eval echo "{1..$num_workers}")
+        do
+            create_node $machine $label $size               
+    done
+else
+    echo "======> Creating $num_workers node"
+    create_node $machine $label $size
+fi
