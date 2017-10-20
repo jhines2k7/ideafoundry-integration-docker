1a76cc7ea331265bbd7e5ee7065c26382983150d
James Hines <jhines2k7@yahoo.com>
2017-10-20 16:28:34 -0400

    Update set-scaling-variables.sh

--- scripts/set-scaling-variables.sh
+++ scripts/set-scaling-variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "export CREATE_PERSON_NODES=$1" >> /root/.profile
-echo "export CREATE_PERSON_NODE_INDEX=$2" >> /root/.profile
+echo "export CREATE_PERSON_NODES=$1" >> /etc/environment
+echo "export CREATE_PERSON_NODE_INDEX=$2" >> /etc/environment
