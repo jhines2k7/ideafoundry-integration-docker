f337bd33b8b5db6d5b58d329979be7e6f8496a84
James Hines <jameshines10@gmail.com>
2017-10-22 19:40:13 -0400

    Updates compose file

--- scripts/remove-nodes.sh
+++ scripts/remove-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all nodes
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'worker\|manager\|mysql\|kafka\|512mb');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'worker\|manager\|mysql\|kafka\|512mb\|1gb');
     do docker-machine rm -f $machine; 
 done
