0be876818aec225c7e1f7ea267aa6a78dd4e80e6
James Hines <jameshines10@gmail.com>
2017-10-21 20:51:32 -0400

    Stuff

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var faker = require('faker');
 
 module.exports = () => {
-    const data = { data: [] }
+    const data = { 
+        orders: {
+            data: []
+        } 
+    }
 
     function randomString(length, chars) {
         var result = '';
@@ -9,7 +13,11 @@ module.exports = () => {
         for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
         
         return result;
-    }    
+    }
+
+    function randomDate(start, end) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString();
+    }
     
     for (let i = 0; i < 50; i++) {
         let outstandingBalance = balance = total = subtotal = price = Math.floor(Math.random() * 200).toFixed(1).toString();
@@ -17,15 +25,17 @@ module.exports = () => {
         let firstName = faker.name.firstName();
         let lastName = faker.name.lastName();
 
-        var verificationCode = randomString(6, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        let verificationCode = randomString(6, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
-        data.data.push({ 
+        let date = randomDate(new Date(2014, 0, 1), new Date());
+
+        data.orders.data.push({ 
             id: i, 
             type: 'orders',
             attributes: {
                 'record_id': Math.floor(Math.random() * 1000000) + 1000000,
                 'coupon_amount': null,
-                'created_at': '2017-10-17T15:12:58.719-04:00',
+                'created_at': date,
                 'description': faker.commerce.productName(),
                 'gift_card_amount': null,
                 'outstanding_balance': outstandingBalance,                
@@ -36,13 +46,14 @@ module.exports = () => {
                 'tax': '0.0',
                 'tax_percentage': '0.0',
                 'total': total,
-                'updated_at': '2017-10-09T20:14:31.595-04:00',
+                'updated_at': date,
                 'verification_code': verificationCode,
                 'balance': balance,
                 'customer_email': faker.internet.email(),
                 'customer_name': `${firstName} ${lastName}`,
                 'customer_first_name': firstName,
-                'customer_last_name': lastName
+                'customer_last_name': lastName,
+                'customer_zip': faker.address.zipCode()
             }
         })
     }
