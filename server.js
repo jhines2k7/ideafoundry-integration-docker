c7192f3d26d1261d1a9f42052bb7c0f2c9f72182
James Hines <jameshines10@gmail.com>
2017-10-22 11:38:25 -0400

    Adds debugger option to server script

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const server = jsonServer.create()
 const router = jsonServer.router()
 const middlewares = jsonServer.defaults()
 
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+})
+
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
