ca85c60ed4f36d793676b941eff0f9ea408dffdd
James Hines <jameshines10@gmail.com>
2017-10-26 18:59:52 -0400

    Changes the name of the directory that contains db schema. Adds a new table to db schema

--- docker/db/ideafoundry.sql
+++ docker/db/ideafoundry.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE IF NOT EXISTS ideafoundrybi;
+CREATE DATABASE IF NOT EXISTS ideafoundry;
  
 USE ideafoundrybi;
  
-CREATE TABLE air_table_person(
+CREATE TABLE person(
    id varchar(255) not null primary key,
    email varchar(255),
    zip varchar(255),
@@ -14,7 +14,7 @@ CREATE TABLE air_table_person(
    updated_at varchar(255)
 )ENGINE=InnoDB;
 
-CREATE TABLE air_table_order(
+CREATE TABLE order(
    id varchar(255) not null primary key,
    person_id varchar(255),
    payment_status varchar(255),
@@ -34,7 +34,7 @@ CREATE TABLE air_table_order(
    FOREIGN KEY fk_person_id(person_id) REFERENCES air_table_person(id) ON UPDATE CASCADE ON DELETE RESTRICT
 )ENGINE=InnoDB;
 
-CREATE TABLE air_table_question(
+CREATE TABLE question(
    id varchar(255) not null primary key,
    answer longtext,
    question longtext,
@@ -46,3 +46,15 @@ CREATE TABLE air_table_question(
    FOREIGN KEY fk_question_person_id(person_id) REFERENCES air_table_person(id) ON UPDATE CASCADE ON DELETE RESTRICT,
    FOREIGN KEY fk_question_order_id(order_id) REFERENCES air_table_order(id) ON UPDATE CASCADE ON DELETE RESTRICT
 )ENGINE=InnoDB;
+
+CREATE TABLE occurence(
+   id varchar(255) not null primary key,
+   closes_at varchar(255),
+   created_at varchar(255),
+   ends_at varchar(255),
+   is_active tinyint(1),
+   schedule_id varchar(255),
+   starts_at varchar(255),
+   time_slot_id varchar(255),
+   updated_at varchar(255)
+)
