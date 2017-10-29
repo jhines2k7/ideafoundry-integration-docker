b24ca92e9e27267d4ba7648dd98a2e5e36880279
James Hines <jameshines10@gmail.com>
2017-10-29 12:15:46 -0400

    Stuff

--- scripts/set-scaling-variables.sh
+++ scripts/set-scaling-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 {
-    printf 'NUM_NODES="%q"\n' "$1"
-    printf 'NODE_INDEX="%q"\n' "$2"
+    echo "NUM_NODES=$1"
+    echo "NODE_INDEX=$2"
 } | sudo tee -a /etc/environment > /dev/null
