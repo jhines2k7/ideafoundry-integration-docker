5057a9589a0adedfc1178cc49fc9a6e3af254490
James Hines <jameshines10@gmail.com>
2017-10-29 09:26:57 -0400

    Removes the word export

--- scripts/set-scaling-variables.sh
+++ scripts/set-scaling-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 {
-    printf 'export NUM_NODES="%q"\n' "$1"
-    printf 'export NODE_INDEX="%q"\n' "$2"
+    printf 'NUM_NODES="%q"\n' "$1"
+    printf 'NODE_INDEX="%q"\n' "$2"
 } | sudo tee -a /etc/environment > /dev/null
