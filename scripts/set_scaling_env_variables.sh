85e50b280f7b845d6f507ec0bcbf667da9f5b56d
James Hines <jhines2k7@yahoo.com>
2017-10-30 16:30:22 -0400

    Update set_scaling_env_variables.sh

--- scripts/set_scaling_env_variables.sh
+++ scripts/set_scaling_env_variables.sh
@@ -1,1 +1,12 @@
+local machine_type=$1
+local num_nodes=$2
+local starting_idx=$3
 
+for machine in $(docker-machine ls --format "{{.Name}}" | grep $machine_type)
+    do
+        echo "======> setting scaling env variables for $machine ..."
+
+        docker-machine ssh $machine 'bash -s' < ./set-scaling-variables.sh $num_nodes $starting_idx            
+
+        ((starting_idx++))
+done
