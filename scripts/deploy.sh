e1492de57d247e8938317cfc3af648764b774517
James Hines <jhines2k7@yahoo.com>
2017-10-30 17:02:19 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ function get_manager_machine_name {
 
 #create manager node
 function create_manager_node {    
-    bash ./create-node.sh manager "node.type=manager" 1gb 1 1
+    bash ./create-node.sh manager "node.type=manager" 1gb 1 0
 
     result=$?
 
@@ -75,7 +75,7 @@ function set_manager_node_env_variables {
 function create_person_worker_nodes {
     local num_nodes=$1
 
-    bash ./create-node.sh createperson "node.type=createperson" 1gb $num_nodes 1
+    bash ./create-node.sh createperson "node.type=createperson" 1gb $num_nodes 0
 
     result=$?
 
@@ -93,7 +93,7 @@ function create_1gb_worker_nodes {
 
     echo "======> creating 1gb worker nodes"
     
-    bash ./create-node.sh 1gb-worker "node.type=1gb-worker" 1gb $num_nodes 1
+    bash ./create-node.sh 1gb-worker "node.type=1gb-worker" 1gb $num_nodes 0
 
     result=$?
 
@@ -109,7 +109,7 @@ function create_1gb_worker_nodes {
 function create_kafka_node {
     echo "======> creating kafka worker node"
 
-    bash ./create-node.sh kafka "node.type=kafka" 2gb 1 1
+    bash ./create-node.sh kafka "node.type=kafka" 2gb 1 0
 
     result=$?
 
@@ -124,7 +124,7 @@ function create_kafka_node {
 function create_mysql_node {
     echo "======> creating mysql worker node"
     
-    bash ./create-node.sh mysql "node.type=mysql" 2gb 1 1
+    bash ./create-node.sh mysql "node.type=mysql" 2gb 1 0
 
     result=$?
 
@@ -184,7 +184,7 @@ function create_512mb_worker_nodes {
 
     echo "======> creating 512mb worker nodes"
     
-    bash ./create-node.sh 512mb-worker "node.type=512mb-worker" 512mb $num_nodes 1
+    bash ./create-node.sh 512mb-worker "node.type=512mb-worker" 512mb $num_nodes 0
 
     result=$?
 
