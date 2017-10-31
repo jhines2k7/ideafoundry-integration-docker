0171794f4c38204b204e0ad1e53c66397e08bfd9
James Hines <jameshines10@gmail.com>
2017-10-31 08:01:19 -0400

    Had to comment out the call to create order worker nodes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -218,7 +218,7 @@ create_manager_node
 init_swarm_manager
 copy_compose_file
 create_person_worker_nodes 4
-create_order_worker_nodes 4
+# create_order_worker_nodes 4
 create_1gb_worker_nodes 1
 create_512mb_worker_nodes 1
 create_mysql_node
