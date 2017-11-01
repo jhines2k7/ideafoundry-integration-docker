fe42b6051ede72520c1eb3728350a16c059768b5
James Hines <jameshines10@gmail.com>
2017-11-01 07:45:19 -0400

    Preparing to test again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -135,8 +135,8 @@ function create_kafka_node {
         then            
             echo "There was an error creating a kafka node."            
     fi
-}
-
+# }
+# 
 function create_mysql_node {
     echo "======> creating mysql worker node"
     
@@ -219,8 +219,8 @@ init_swarm_manager
 copy_compose_file
 # create_person_worker_nodes 4
 # create_order_worker_nodes 4
-create_1gb_worker_nodes 1
-create_512mb_worker_nodes 1
+# create_1gb_worker_nodes 1
+# create_512mb_worker_nodes 1
 create_mysql_node
 create_kafka_node
 
