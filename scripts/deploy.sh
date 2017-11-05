4303677d100a292e3557232c91f2a3cd30102c41
James Hines <jameshines10@gmail.com>
2017-11-04 22:07:01 -0400

    Kafka, mysql

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -202,8 +202,8 @@ create_manager_node
 init_swarm_manager
 copy_compose_file
 # create_person_worker_nodes 5
-create_1gb_worker_nodes 1
-create_512mb_worker_nodes 1
+# create_1gb_worker_nodes 1
+# create_512mb_worker_nodes 1
 create_mysql_node
 create_kafka_node
 
