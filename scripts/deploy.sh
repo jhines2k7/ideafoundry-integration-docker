432316c4b804b250ada9dcbebcf636b517d2bf6f
James Hines <jameshines10@gmail.com>
2017-11-14 07:47:59 -0500

    Number of createperson instances is set by instance count env variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -183,7 +183,7 @@ init_swarm_manager
 copy_compose_file
 create_kafka_node
 create_mysql_node
-create_person_worker_nodes 2
+create_person_worker_nodes $INSTANCE_COUNT
 create_1gb_worker_nodes 1
 create_512mb_worker_nodes 1
 
