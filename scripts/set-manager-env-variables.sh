8174fa5e68bd1ce1d0fd82a922276f62d3e5f2e9
James Hines <jameshines10@gmail.com>
2017-11-21 06:00:12 -0500

    Changes the env variables script to use the echo command

--- scripts/set-manager-env-variables.sh
+++ scripts/set-manager-env-variables.sh
@@ -3,25 +3,25 @@
 echo "=======> setting env variables for manager node"
 
 {
-    printf 'DB_HOST="%q"\n' "$1"
-    printf 'KAFKA_HOST="%q"\n' "$2"
-    printf 'ZOOKEEPER_HOST="%q"\n' "$3"
-    printf 'OKHTTP_CLIENT_TIMEOUT_SECONDS="%q"\n' "$4"
-    printf 'AIRTABLE_APP_ID="%q"\n' "$5"
-    printf 'OCCASION_EXPORT_STARTING_PAGE_NUM="%q"\n' "$6"
-    printf 'IF_OCCASION_EXPORT_URL="%q"\n' "$7"
-    printf 'IF_AIRTABLE_CREDS="%q"\n' "$8"
-    printf 'IF_DB_PASSWORD="%q"\n' "$9"
-    printf 'IF_DB_PORT="%q"\n' "${10}"
-    printf 'IF_DB_ROOT_PASS="%q"\n' "${11}"
-    printf 'IF_DB_USERNAME="%q"\n' "${12}"
-    printf 'IF_EMAIL_CREDS="%q"\n' "${13}"
-    printf 'IF_EMAIL_ID="%q"\n' "${14}"
-    printf 'IF_OCCASION_CREDS="%q"\n' "${15}"
-    printf 'GIT_USERNAME="%q"\n' "${16}"
-    printf 'GIT_PASSWORD="%q"\n' "${17}"
-    printf 'DOCKER_HUB_USER="%q"\n' "${18}"
-    printf 'DOCKER_HUB_PASSWORD="%q"\n' "${19}"
-    printf 'DIGITALOCEAN_ACCESS_TOKEN="%q"\n' "${20}"
-    printf 'INSTANCE_COUNT="%q"\n' "${21}"
+    echo 'DB_HOST="$1"\n'
+    echo 'KAFKA_HOST="$2"\n'
+    echo 'ZOOKEEPER_HOST="$3"\n'
+    echo 'OKHTTP_CLIENT_TIMEOUT_SECONDS="$4"\n'
+    echo 'AIRTABLE_APP_ID="$5"\n'
+    echo 'OCCASION_EXPORT_STARTING_PAGE_NUM="$6"\n'
+    echo 'IF_OCCASION_EXPORT_URL="$7"\n'
+    echo 'IF_AIRTABLE_CREDS="$8"\n'
+    echo 'IF_DB_PASSWORD="$9"\n'
+    echo 'IF_DB_PORT="${10}"\n'
+    echo 'IF_DB_ROOT_PASS="${11}"\n'
+    echo 'IF_DB_USERNAME="${12}"\n'
+    echo 'IF_EMAIL_CREDS="${13}"\n'
+    echo 'IF_EMAIL_ID="${14}"\n'
+    echo 'IF_OCCASION_CREDS="${15}"\n'
+    echo 'GIT_USERNAME="${16}"\n'
+    echo 'GIT_PASSWORD="${17}"\n'
+    echo 'DOCKER_HUB_USER="${18}"\n'
+    echo 'DOCKER_HUB_PASSWORD="${19}"\n'
+    echo 'DIGITALOCEAN_ACCESS_TOKEN="${20}"\n'
+    echo 'INSTANCE_COUNT="${21}"\n'
 } | sudo tee -a /etc/environment > /dev/null
