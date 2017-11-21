14bb3fd67d97d1e19fa1423b7a20ecb848147780
James Hines <jameshines10@gmail.com>
2017-11-21 07:38:07 -0500

    Fixes some things

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,15 +37,15 @@ function set_manager_node_env_variables {
 
     if [ "$ENV" = "dev" ]
     then
-#        kafka_machine_ip=$(get_ip $(docker-machine ls --format "{{.Name}}" | grep 'kafka'))
-#        mysql_host=$(get_ip $(docker-machine ls --format "{{.Name}}" | grep 'mysql'))
+        kafka_machine_ip=$(get_ip $(docker-machine ls --format "{{.Name}}" | grep 'kafka'))
+        mysql_host=$(get_ip $(docker-machine ls --format "{{.Name}}" | grep 'mysql'))
 
-        # kafka_host=$kafka_machine_ip
-        # zookeeper_host=$kafka_machine_ip
+        kafka_host=$kafka_machine_ip
+        zookeeper_host=$kafka_machine_ip
 
-        mysql_host="mysql"
-        kafka_host="kafka"
-        zookeeper_host="zookeeper"
+#        mysql_host="mysql"
+#        kafka_host="kafka"
+#        zookeeper_host="zookeeper"
     fi
 
     ./runremote.sh \
@@ -215,6 +215,6 @@ set_manager_node_env_variables
 
 deploy_stack
 
-scale_createperson_nodes
+#scale_createperson_nodes
 
 docker-machine ls
