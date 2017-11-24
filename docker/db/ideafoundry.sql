d20b9b4993993bc0f1711108b74b4689b138de11
Jhines <jhines@contractor.lb.com>
2017-11-24 10:41:24 -0500

    Creates a docker file for reconcile question jdbc sink

--- docker/db/ideafoundry.sql
+++ docker/db/ideafoundry.sql
@@ -1,99 +1,143 @@
+# noinspection SqlNoDataSourceInspectionForFile
+
 CREATE DATABASE IF NOT EXISTS ideafoundry;
- 
+
 USE ideafoundry;
- 
-CREATE TABLE person(
-    id int(11) not null primary key,
-    occasion_id varchar(255),
-    email varchar(255),
-    address varchar(255),
-    zip varchar(255),
-    city varchar(255),
-    phone varchar(255),
-    first_name varchar(255),
-    last_name varchar(255),
-    full_name varchar(255),
-    created_at varchar(255),
-    updated_at varchar(255)
-)ENGINE=InnoDB;
 
-CREATE TABLE occasion_order(
-    id int(11) not null primary key,
-    occasion_id varchar(255),
-    person_id int(11),
-    gift_card_amount double,
-    coupon_amount double,
-    outstanding_balance double,
-    subtotal double,
-    total double,
-    coupon_description varchar(255),
-    tax_percentage double,
-    payment_status varchar(255),
-    status varchar(255),
-    balance double,
-    price double,
-    description varchar(255),
-    verification_code varchar(255),
-    quantity int(11),
-    tax double,
-    created_at varchar(255),
-    updated_at varchar(255),
-    FOREIGN KEY fk_person_id(person_id) REFERENCES person(id) ON UPDATE CASCADE ON DELETE RESTRICT
-)ENGINE=InnoDB;
+CREATE TABLE person (
+  id          INT(11) NOT NULL PRIMARY KEY,
+  occasion_id VARCHAR(255),
+  email       VARCHAR(255),
+  address     VARCHAR(255),
+  zip         VARCHAR(255),
+  city        VARCHAR(255),
+  phone       VARCHAR(255),
+  first_name  VARCHAR(255),
+  last_name   VARCHAR(255),
+  full_name   VARCHAR(255),
+  created_at  VARCHAR(255),
+  updated_at  VARCHAR(255)
+)
+  ENGINE = InnoDB;
 
-CREATE TABLE question(
-    id int(11) not null primary key,
-    occasion_id varchar(255),
-    question longtext,
-    answer longtext,
-    created_at varchar(255),
-    updated_at varchar(255)
-)ENGINE=InnoDB;
+CREATE TABLE occasion_order (
+  id                  INT(11) NOT NULL PRIMARY KEY,
+  occasion_id         VARCHAR(255),
+  person_id           INT(11),
+  gift_card_amount    DOUBLE,
+  coupon_amount       DOUBLE,
+  outstanding_balance DOUBLE,
+  subtotal            DOUBLE,
+  total               DOUBLE,
+  coupon_description  VARCHAR(255),
+  tax_percentage      DOUBLE,
+  payment_status      VARCHAR(255),
+  status              VARCHAR(255),
+  balance             DOUBLE,
+  price               DOUBLE,
+  description         VARCHAR(255),
+  verification_code   VARCHAR(255),
+  quantity            INT(11),
+  tax                 DOUBLE,
+  created_at          VARCHAR(255),
+  updated_at          VARCHAR(255),
+  FOREIGN KEY fk_person_id(person_id) REFERENCES person (id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+)
+  ENGINE = InnoDB;
 
-CREATE TABLE occurrence(
-    id int(11) not null primary key,
-    occasion_id varchar(255),
-    order_id int(11),
-    duration int(11),
-    closes_at varchar(255),
-    created_at varchar(255),
-    ends_at varchar(255),
-    is_active tinyint(1),
-    schedule_id varchar(255),
-    starts_at varchar(255),
-    time_slot_id varchar(255),
-    updated_at varchar(255),
-    FOREIGN KEY fk_order_id(order_id) REFERENCES occasion_order(id) ON UPDATE CASCADE ON DELETE RESTRICT
-)ENGINE=InnoDB;
+CREATE TABLE question (
+  id          INT(11) NOT NULL PRIMARY KEY,
+  occasion_id VARCHAR(255),
+  question    LONGTEXT,
+  answer      LONGTEXT,
+  created_at  VARCHAR(255),
+  updated_at  VARCHAR(255)
+)
+  ENGINE = InnoDB;
 
-create table order_question(
-    order_id int(11),
-    question_id int(11),
-    CONSTRAINT order_question_pk PRIMARY KEY (order_id, question_id),
-    CONSTRAINT fk_order_question_order FOREIGN KEY (order_id) REFERENCES occasion_order (id),
-    CONSTRAINT fk_order_question_question FOREIGN KEY (question_id) REFERENCES question (id)
-)ENGINE=InnoDB;
+CREATE TABLE occurrence (
+  id           INT(11) NOT NULL PRIMARY KEY,
+  occasion_id  VARCHAR(255),
+  order_id     INT(11),
+  duration     INT(11),
+  closes_at    VARCHAR(255),
+  created_at   VARCHAR(255),
+  ends_at      VARCHAR(255),
+  is_active    TINYINT(1),
+  schedule_id  VARCHAR(255),
+  starts_at    VARCHAR(255),
+  time_slot_id VARCHAR(255),
+  updated_at   VARCHAR(255),
+  FOREIGN KEY fk_order_id(order_id) REFERENCES occasion_order (id)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+)
+  ENGINE = InnoDB;
 
-create table person_question(
-    person_id int(11),
-    question_id int(11),
-    CONSTRAINT person_question_pk PRIMARY KEY (person_id, question_id),
-    CONSTRAINT fk_person_question_person FOREIGN KEY (person_id) REFERENCES person (id),
-    CONSTRAINT fk_person_question_question FOREIGN KEY (question_id) REFERENCES question (id)
-)ENGINE=InnoDB;
+CREATE TABLE reconcile_question (
+  id        VARCHAR(255) NOT NULL PRIMARY KEY,
+  record_id INT(11),
+  question  LONGTEXT,
+  answer    LONGTEXT
+)
+  ENGINE = InnoDB;
 
-create table person_occurrence(
-    person_id int(11),
-    occurrence_id int(11),
-    CONSTRAINT person_occurrence_pk PRIMARY KEY (person_id, occurrence_id),
-    CONSTRAINT fk_person_occurrence_person FOREIGN KEY (person_id) REFERENCES person (id),
-    CONSTRAINT fk_person_occurrence_occurrence FOREIGN KEY (occurrence_id) REFERENCES occurrence (id)
-)ENGINE=InnoDB;
+CREATE TABLE reconcile_order (
+  id          VARCHAR(255) NOT NULL PRIMARY KEY,
+  record_id   INT(11),
+  description LONGTEXT
+)
+  ENGINE = InnoDB;
 
-create table occurrence_question(
-    occurrence_id int(11),
-    question_id int(11),
-    CONSTRAINT occurrence_question_pk PRIMARY KEY (occurrence_id, question_id),
-    CONSTRAINT fk_occurrence_question_occurrence FOREIGN KEY (occurrence_id) REFERENCES occurrence (id),
-    CONSTRAINT fk_occurrence_question_question FOREIGN KEY (question_id) REFERENCES question (id)
-)ENGINE=InnoDB;
+CREATE TABLE reconcile_person (
+  id         VARCHAR(255) NOT NULL PRIMARY KEY,
+  record_id  INT(11),
+  first_name VARCHAR(255),
+  last_name  VARCHAR(255)
+)
+  ENGINE = InnoDB;
+
+CREATE TABLE reconcile_occurrence (
+  id        VARCHAR(255) NOT NULL PRIMARY KEY,
+  record_id INT(11),
+  starts_at VARCHAR(255)
+)
+  ENGINE = InnoDB;
+
+CREATE TABLE order_question (
+  order_id    INT(11),
+  question_id INT(11),
+  CONSTRAINT order_question_pk PRIMARY KEY (order_id, question_id),
+  CONSTRAINT fk_order_question_order FOREIGN KEY (order_id) REFERENCES occasion_order (id),
+  CONSTRAINT fk_order_question_question FOREIGN KEY (question_id) REFERENCES question (id)
+)
+  ENGINE = InnoDB;
+
+CREATE TABLE person_question (
+  person_id   INT(11),
+  question_id INT(11),
+  CONSTRAINT person_question_pk PRIMARY KEY (person_id, question_id),
+  CONSTRAINT fk_person_question_person FOREIGN KEY (person_id) REFERENCES person (id),
+  CONSTRAINT fk_person_question_question FOREIGN KEY (question_id) REFERENCES question (id)
+)
+  ENGINE = InnoDB;
+
+CREATE TABLE person_occurrence (
+  person_id     INT(11),
+  occurrence_id INT(11),
+  CONSTRAINT person_occurrence_pk PRIMARY KEY (person_id, occurrence_id),
+  CONSTRAINT fk_person_occurrence_person FOREIGN KEY (person_id) REFERENCES person (id),
+  CONSTRAINT fk_person_occurrence_occurrence FOREIGN KEY (occurrence_id) REFERENCES occurrence (id)
+)
+  ENGINE = InnoDB;
+
+CREATE TABLE occurrence_question (
+  occurrence_id INT(11),
+  question_id   INT(11),
+  CONSTRAINT occurrence_question_pk PRIMARY KEY (occurrence_id, question_id),
+  CONSTRAINT fk_occurrence_question_occurrence FOREIGN KEY (occurrence_id) REFERENCES occurrence (id),
+  CONSTRAINT fk_occurrence_question_question FOREIGN KEY (question_id) REFERENCES question (id)
+)
+  ENGINE = InnoDB;
