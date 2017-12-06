fa8a0b71d437aff007b9f35a5543782b729008dc
Jhines <jhines@contractor.lb.com>
2017-12-06 10:30:50 -0500

    Updates the script that removes nodes with failed docker installs

--- scripts/remove-nodes-with-failed-docker-installations.sh
+++ scripts/remove-nodes-with-failed-docker-installations.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-file="./failed_installs.txt"
+failed_installs_file="./failed_installs.txt"
 
-if [[ -s $file ]] ; then
+if [[ -s $failed_installs_file ]] ; then
     echo "======> removing machines with failed docker installations ..."
 
     while read machine || [[ -n $machine ]] ; do
         docker-machine rm -f $machine
-    done < $file
-    
+    done < $failed_installs_file
+
+    > $failed_installs_file
 else
     echo "======> there were no machines with failed docker installations ..."
 fi ;
