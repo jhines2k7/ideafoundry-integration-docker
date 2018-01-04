c034b3a00e3e2f83e4cd192635342efc51e852c1
Jhines <jhines@contractor.lb.com>
2018-01-04 14:27:44 -0500

    Fixing...

--- scripts/deploy-stack.sh
+++ scripts/deploy-stack.sh
@@ -4,6 +4,7 @@ manager_machine=$(docker-machine ls --format "{{.Name}}" | grep 'manager')
 
 docker_file="docker-compose.yml"
 directory=/
+env_file=".env"
 
 if [ "$PROVIDER" = "aws" ] && [ "$ENV" = "dev" ]
 then
@@ -36,8 +37,8 @@ fi
 docker-machine ssh $manager_machine sudo docker login --username=$DOCKER_HUB_USER --password=$DOCKER_HUB_PASSWORD
 
 docker-machine ssh $manager_machine \
-    export $(cat .env) && \
-    sudo docker stack deploy \
+    export $(cat $directory$env_file) \
+    && sudo docker stack deploy \
     --compose-file $directory$docker_file \
     --with-registry-auth \
     integration
