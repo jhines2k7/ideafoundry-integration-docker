ebd9f360db81b83d398ce9042832ebbf67f04c81
James Hines <jameshines10@gmail.com>
2018-01-23 07:40:26 -0500

    Removes uneeded script. Removes mysql reference from remove-all-nodes-script

--- scripts/remove-all-but-mysql-node.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#remove all but mysql node
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|kafka\|512mb\|1gb\|save');
-    do docker-machine rm -f $machine; 
-done
