f852ff9f8cbdae87408bd09beb5a42dd42e6dc40
James Hines <jameshines10@gmail.com>
2018-01-26 08:12:06 -0500

    Adds informative logging before the removal of mysql service from non immutable infrastructure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,5 +34,6 @@ docker-machine ssh $manager_machine \
 
 if [ "$IMMUTABLE_INFRASTRUCTURE" = false ]
 then
+    echo "======> removing mysql service from swarm"
     docker-machine ssh $manager_machine sudo docker service rm integration_mysql
 fi
