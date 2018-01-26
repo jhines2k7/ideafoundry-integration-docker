a73a5d318bd0d5b1f50bfd7adcbaf7bf3de52393
James Hines <jameshines10@gmail.com>
2018-01-26 05:26:29 -0500

    Forgot to add a pattern that will match mysql databases when removing all nodes

--- scripts/remove-all-nodes.sh
+++ scripts/remove-all-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #remove all nodes
-for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|kafka\|512mb\|1gb\|save');
+for machine in $(docker-machine ls --format "{{.Name}}" | grep 'manager\|kafka\|512mb\|1gb\|save\|mysql');
     do docker-machine rm -f $machine;
 done
